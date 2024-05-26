fde3143c2d00f3a74e8816127a72d2b8fa4ec8dc
Тажиев Алмас <almas200224@gmail.com>
2024-05-27 00:29:58 +0600

    car

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Car from "./block-element/Car.jsx";
+import "normalize.css";
+import "./index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Car />
+  </React.StrictMode>
+);
